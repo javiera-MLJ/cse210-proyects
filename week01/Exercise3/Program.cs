6bffd87a1ec59ccc7bca0e01d8bdb9543c4ef6ca
javiera-MLJ <lor24003@byui.edu>
2025-05-09 19:14:37 -0400

    prep 3 complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,11 +1,46 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your grade percentage? ");
-        string userInput = Console.ReadLine();
-        int grade = int.Parse(userInput);
+
+
+        Random randomGenerator = new Random();
+        int mNumber = randomGenerator.Next(1, 1000);
+    
+        int guess = -1;
+        int attempts = 0;
+
+        while (guess != mNumber)
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+
+            for (int i = 0; i < 1; i++)
+            {
+                attempts++;
+            }
+
+            if (guess > mNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+    
+            else if (guess < mNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+    
+        }
+
+            Console.WriteLine($"you have made {attempts} attempts");
     }
 }
