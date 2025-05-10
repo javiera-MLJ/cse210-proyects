4766a8d63e0a4f6627854d6c80c7ff19d413f764
javiera-MLJ <lor24003@byui.edu>
2025-05-10 01:36:32 -0400

    prep 4 complete

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Globalization;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int number = -1;
+        int sum = 0;
+        int average = 0;
+
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        if (numbers.Count > 0)
+        {
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            average = sum / numbers.Count;
+            int max = numbers.Max();
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+        }
     }
 }
