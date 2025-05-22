36abab2d6e0f61c51923eedfd592e9e6f3762724
javiera-MLJ <lor24003@byui.edu>
2025-05-21 21:36:37 -0400

    prep week 3 complete

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Console.WriteLine("what is the top number of the fraction?");
+        int top = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("what is the bottom number of the fraction?");
+        int bottom = int.Parse(Console.ReadLine());
+
+        Fraction fraction = new Fraction();
+        fraction.SetTop(top);
+        fraction.SetBottom(bottom);
+        Console.WriteLine($"fraction: {fraction.GetFractionString()}");
+        Console.WriteLine($"decimal: {fraction.GetDecimalValue()}");
     }
 }
