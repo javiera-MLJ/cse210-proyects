2200b30b418b3c32bdb686f89ba5c823b210cf7a
javiera-MLJ <lor24003@byui.edu>
2025-06-13 13:42:40 -0400

    shape program complete

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Pink", 4);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("black", 6, 8);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Purple", 2.5);
+        shapes.Add(circle);
+        
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,21 @@
+public abstract class Shape
+{
+    private string _color;
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    private void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+}
